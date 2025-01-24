1b0502e1e2c822ed70555c508fb74c04541450c1
Marcos Esteban <83374363+MarcosEstebanDev@users.noreply.github.com>
2025-01-24 01:11:10 -0300

    update

--- django_snippets/wsgi.py
+++ django_snippets/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
+
 import os
+import sys
+
+path = '/home/MarcosEsDev/snippet'  # Ruta a tu proyecto
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'snippet.settings'  # Cambia 'snippet' por el nombre de tu proyecto
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_snippets.settings")
-
 application = get_wsgi_application()
